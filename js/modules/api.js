dec1794c357498db180066ef005f3b3deb437b7f
qiubee <qiubees@hotmail.com>
2020-03-05 20:54:22 +0100

    add parameter to api module

--- js/modules/api.js
+++ js/modules/api.js
@@ -1,15 +1,12 @@
-const cors = 'https://cors-anywhere.herokuapp.com/';
-const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
-const query = 'boek';
-const key = 'd7519ea81ad4e06ab5e5dac46ddeb63a';
-const secret = '274658a302d1cfe874e73aed9d6ccef5';
-const detail = 'Default';
-const fullURL = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
+const cors = "https://cors-anywhere.herokuapp.com/";
+const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
+const key = "d7519ea81ad4e06ab5e5dac46ddeb63a";
+const secret = "274658a302d1cfe874e73aed9d6ccef5";
+const detail = "Default";
 
 const config = {
   Authorization: `Bearer ${secret}`
 };
-
 async function fetchData(url) {
   const res = await fetch(url);
   if (res.ok) {
@@ -20,4 +17,7 @@ async function fetchData(url) {
   }
 }
 
-export async function getOBAData() {return await fetchData(fullURL, config);}
+export async function getBooks(query) {
+  const fullUrl = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
+  return await fetchData(fullUrl, config);
+}
